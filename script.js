e60e4f8f7f18439cd93ab5d0712122696a754eb5
forjadasnolugarsecreto-devocional <forjadasnolugarsecreto@gmail.com>
2026-07-17 17:25:24 -0300

    Update playlist titles and improve DOM handling

--- script.js
+++ script.js
@@ -1,68 +1,137 @@
-
 const playlist = [
 
 {
-title:"Música 1",
+title:"Canção & Louvor | Eu Nunca Fui Forte",
 id:"Sg0_mFLVqZo"
 },
 
 {
-title:"Música 2",
+title:"Stella Laura | O Senhor é o Meu Pastor",
 id:"WTen9x3j6Sw"
 },
 
 {
-title:"Música 3",
+title:"Débora Almeida | Testemunho Lindo",
 id:"dNMIxWWxYNo"
 },
 
 {
-title:"Música 4",
-id:"QYKpysFX-9I"
+title:"Kailane Frauches | No Secreto",
+id:"ID_DO_VIDEO"
 },
 
 {
-title:"Música 5",
-id:"66BP5rpxydo"
+title:"Rebeca Carvalho & Leandro Borges - Você Viverá",
+id:"ID_DO_VIDEO"
 },
 
 {
-title:"Música 6",
-id:"Kv4eCXia5fQ"
+title:"Oração Forjadas no Lugar Secreto",
+id:"ID_DO_VIDEO"
 }
 
 ];
 
-const container=document.getElementById("playlist");
 
-const iframe=document.getElementById("player");
+let player;
 
-const title=document.getElementById("currentTitle");
+let current=0;
 
-playlist.forEach((music,index)=>{
 
-const btn=document.createElement("button");
+function onYouTubeIframeAPIReady(){
 
-btn.className="music";
+player = new YT.Player('player',{
 
-btn.innerText=music.title;
+height:'120',
 
-btn.onclick=()=>{
+width:'100%',
 
-document.querySelectorAll(".music").forEach(item=>item.classList.remove("active"));
+videoId:playlist[0].id,
 
-btn.classList.add("active");
-
-title.innerText=music.title;
-
-iframe.src=`https://www.youtube.com/embed/${music.id}?autoplay=1`;
-
-};
-
-container.appendChild(btn);
+playerVars:{
+controls:1,
+rel:0
+}
 
 });
 
-btn=container.children[0];
+}
 
-btn.click();
+
+const list=document.getElementById("playlist");
+
+
+playlist.forEach((music,index)=>{
+
+let div=document.createElement("div");
+
+div.className="music";
+
+div.innerHTML=music.title;
+
+
+div.onclick=()=>{
+
+current=index;
+
+player.loadVideoById(music.id);
+
+document.getElementById("current").innerHTML=music.title;
+
+};
+
+
+list.appendChild(div);
+
+
+});
+
+
+
+function playPause(){
+
+let state=player.getPlayerState();
+
+if(state===1){
+
+player.pauseVideo();
+
+}else{
+
+player.playVideo();
+
+}
+
+}
+
+
+
+function next(){
+
+current++;
+
+if(current>=playlist.length)
+current=0;
+
+
+player.loadVideoById(playlist[current].id);
+
+document.getElementById("current").innerHTML=playlist[current].title;
+
+}
+
+
+
+function previous(){
+
+current--;
+
+if(current<0)
+current=playlist.length-1;
+
+
+player.loadVideoById(playlist[current].id);
+
+document.getElementById("current").innerHTML=playlist[current].title;
+
+}
